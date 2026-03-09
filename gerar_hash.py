418530f4dfcd5310f20d56c5bcdf2c85e4fdb008
Guilherme Arruda <guiga.arruda@hotmail.com>
2026-03-09 17:53:30 +0100

    test 2

--- gerar_hash.py
+++
@@ -1,8 +0,0 @@
-import streamlit_authenticator as stauth
-
-# Senhas em texto puro
-senhas = ["admin02182604", "admin02182604"]
-
-# Gerar hashes
-hashed = stauth.Hasher(senhas).generate()
-print(hashed)
